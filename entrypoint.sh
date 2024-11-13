ad45ce3c57aaa7b16ea64bff5929dad532f37037
Highbyte <highbyte@outlook.com>
2024-11-13 17:31:02 +0100

    Update .NET 8 SDK to 8.0.403. Update to SonarScanner v9.0.1 Update to Node v22

--- entrypoint.sh
+++ entrypoint.sh
@@ -106,6 +106,9 @@ if [[ $GITHUB_EVENT_NAME == 'pull_request' ]]; then
 
 fi
 
+#Skip JRE provisioning in SonarScanner for MSBuild v7.0+. Instead use the JRE provided by the Docker image (which must be in the PATH).
+sonar_begin_cmd="$sonar_begin_cmd /d:sonar.scanner.skipJreProvisioning=true"
+
 #-----------------------------------
 # Build Sonarscanner end command
 #-----------------------------------
